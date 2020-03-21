fb1f5ae9d9a55931d0701d599f234ab9541841d6
Sergey Bulatnikov <bofa111@gmail.com>
2020-03-21 14:49:50 +0300

    Added: + .gitignore file + LW2/hello_world + LW2/print_args + LW2/remove_duplicates

---
+++ LW2/print_args/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace print_args
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (CheckArgs(args) == 1)
+            {
+                Environment.Exit(1);
+            }
+            Console.WriteLine("Number of arguments: " + args.Length);
+            PrintArgs(args);
+        }
+
+        private static int CheckArgs(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                Console.WriteLine("No parameters were specified!");
+                return 1;
+            }
+            return 0;
+        }
+
+        private static void PrintArgs(string[] args)
+        {
+            Console.Write("Arguments: ");
+            for (long i = 0; i < args.Length; i++)
+            {
+                Console.WriteLine(args[i]);
+            }
+        }
+    }
+}
---
+++ LW2/remove_duplicates/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace remove_duplicates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (!CheckArgs(args))
+            {
+                Environment.Exit(1);
+            }
+
+            string line;
+            line = GetLine(args);
+
+            if (line.Length == 0)
+            {
+                Console.WriteLine();
+            }
+            else
+            {
+                RemoveDuplicates(line);
+            }
+        }
+        private static void RemoveDuplicates(string someLine)
+        {
+            string noDuplicates = "";
+            for (int i = 0; i < someLine.Length; i++)
+            {
+                if (!noDuplicates.Contains(someLine[i]))
+                {
+                    noDuplicates += someLine[i];
+                }
+            }
+            Console.WriteLine(noDuplicates);
+        }
+
+        private static bool CheckArgs(string[] args)
+        {
+            if (args.Length != 1)
+            {
+                Console.WriteLine("Incorrect number of arguments!");
+                Console.WriteLine("Usage remove_duplicates.exe <input string>");
+                return false;
+            }
+            return true;
+        }
+
+        private static string GetLine(string[] args)
+        {
+            return args[0];
+        }
+    }
+}
